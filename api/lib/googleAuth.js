17ab50e50443e67028835833295f72bb8791dc91
WillCureton76 <willcureton@gmail.com>
2025-10-08 19:15:24 +0100

    Create googleAuth.js

---
+++ api/lib/googleAuth.js
@@ -0,0 +1,16 @@
+export async function getAccessToken() {
+  const resp = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID,
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+      grant_type: "refresh_token",
+    }),
+  });
+
+  const json = await resp.json();
+  if (!resp.ok) throw new Error(JSON.stringify(json));
+  return json.access_token;
+}
